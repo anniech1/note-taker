5f4345f0725018c69e245b944b54c0ec4587ae87
anniech1 <ach577@nyu.edu>
2022-09-05 23:21:52 -0400

    added api get routes and html get routes

--- server.js
+++ server.js
@@ -7,36 +7,65 @@ const PORT = process.env.port || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 // html get routes
-app.get('/notes', (req, res) =>
+app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+});
+
 
 app.get('*',(req,res)=> {
     res.sendFile(path.join(__dirname, '/public/index.html'))
   });
 
 
-// api get and post routes
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-          console.log("error with reading db.json!");
-        } else {
-          const notes = JSON.parse(data); 
-        }})
+// api get routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'public/index.html'));
 });
 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname,'public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname,'db/db.json'));
+});
+
+app.get("/api/notes/:note", function(req, res) {
+  var note = req.params.note;
+  // console.log(note);
+  res.json(note)      
+});
+
+// app.get('/api/notes', (req, res) => {
+//     fs.readFile('./db/db.json', 'utf8', (err, data) => {
+//         if (err) {
+//           console.log("error with reading db.json!");
+//         } else {
+//           const notes = JSON.parse(data); 
+//         }})
+// });
+
+//api post routes
 app.post('/api/notes', (req, res) => {
     // const newNote = {
     //     title,
     //     text
     // };
-    const newNote = createNewNote(req.body);
-        res.json(newNote);
+    // const newNote = createNewNote(req.body);
+    //     res.json(newNote);
+    const addedNote = req.body;
+    addedNote.id = Date.now();
 });
 
 
+
+//start server
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
 );
