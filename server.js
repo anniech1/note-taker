b41050d229b97da74e40d0328b479375455ec2a9
anniech1 <ach577@nyu.edu>
2022-09-05 23:05:56 -0400

    added middleware

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const fs = require('fs');
 const PORT = process.env.port || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // html get routes
 app.get('/notes', (req, res) =>
@@ -16,13 +18,28 @@ app.get('*',(req,res)=> {
 
 
 // api get and post routes
-
 app.get('/api/notes', (req, res) => {
-    // Send a message to the client
-    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
-          console.error(err);
+          console.log("error with reading db.json!");
         } else {
-          const notes = JSON.parse(data);
+          const notes = JSON.parse(data); 
         }})
+});
+
+app.post('/api/notes', (req, res) => {
+    // const newNote = {
+    //     title,
+    //     text
+    // };
+    const newNote = createNewNote(req.body);
+        res.json(newNote);
+});
+
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
   
+
+    
